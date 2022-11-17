c3896147bd593cd3890528f393e0bec409972f71
thinleyjimmydorji <thinleyjimmy123@gmail.com>
2022-11-17 22:48:55 +0600

    initial commit

---
+++ apps/new-rentals/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'new-rentals-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'new-rentals';
+}
