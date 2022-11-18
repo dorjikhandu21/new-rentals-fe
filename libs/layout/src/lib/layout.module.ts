dafe7eaf94e8e26f9670dcfc64b51d0b3c15ecf4
thinleyjimmydorji <thinleyjimmy123@gmail.com>
2022-11-18 21:13:00 +0600

    feat(newrentals): maps

--- libs/layout/src/lib/layout.module.ts
+++ libs/layout/src/lib/layout.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './components/layout/layout.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import {GoogleMapsModule} from "@angular/google-maps";
 
 @NgModule({
-    imports: [CommonModule],
+  imports: [CommonModule, GoogleMapsModule],
     declarations: [LayoutComponent, NavBarComponent, FooterComponent],
     exports: [
         LayoutComponent
