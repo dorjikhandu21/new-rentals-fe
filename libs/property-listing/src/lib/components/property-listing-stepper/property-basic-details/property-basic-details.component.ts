b5de88cb840d4512bffc8e6d431818d2389ae98f
thinleyjimmydorji <thinleyjimmy123@gmail.com>
2022-11-24 15:37:08 +0600

    fix(newrentals): file picker

--- libs/property-listing/src/lib/components/property-listing-stepper/property-basic-details/property-basic-details.component.ts
+++ libs/property-listing/src/lib/components/property-listing-stepper/property-basic-details/property-basic-details.component.ts
@@ -11,20 +11,34 @@ import {CategoryEnum} from "@new-rentals/shared";
 export class PropertyBasicDetailsComponent implements OnInit {
   @Input() basicDetail?: FormGroup;
   @ViewChild('fileUpload', {static: false}) fileUpload?: ElementRef;
+  imageUrls: {url: string, fileName: string }[] = [];
   constructor(private propertyFacadeService: PropertyFacadeService) {}
 
   ngOnInit(): void {
   }
 
   selectFile(event: Event): void {
+    const reader: FileReader = new FileReader();
+    let fileUrl: string;
+    // @ts-ignore
+    reader.readAsDataURL(event?.target?.['files']?.[0]);
+    reader.onload = (events:ProgressEvent<FileReader>): void => {
+      fileUrl = events?.target?.result as string;
+      this.addToControls(event, fileUrl);
+    }
+  }
+
+  addToControls(event: Event, fileUrl: string): void {
     const attachments: FormArray = this.basicDetail?.get('attachments') as FormArray;
     const attachmentControl: FormGroup = new FormGroup({
       category: new FormControl(CategoryEnum.Property),
       // @ts-ignore
       file: new FormControl(event?.target?.['files']?.[0]),
       // @ts-ignore
-      fileFileName: new FormControl(event?.target?.['files']?.[0]['name'])
+      fileFileName: new FormControl(event?.target?.['files']?.[0]['name']),
+      url: new FormControl(fileUrl)
     })
     attachments.push(attachmentControl);
   }
+
 }
