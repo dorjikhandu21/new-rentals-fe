c158e2a07a97e79bf4f8f2997d5b9f80c5cb6d44
thinleyjimmydorji <thinleyjimmy123@gmail.com>
2022-11-26 16:37:55 +0600

    fix(newrentals): draggable markers

--- libs/property-listing/src/lib/components/property-listing-stepper/property-location/property-location.component.ts
+++ libs/property-listing/src/lib/components/property-listing-stepper/property-location/property-location.component.ts
@@ -11,12 +11,14 @@ import {Loader} from "@googlemaps/js-api-loader";
 export class PropertyLocationComponent implements OnInit {
   @Input() geoInformation?:FormGroup;
   private map: google.maps.Map;
+  markers: google.maps.Marker[] = [];
   position: { lat: number; lng: number; } = {lat: 27.4716, lng: 89.6386};
   center!: google.maps.LatLngLiteral;
   options: google.maps.MapOptions = {
+    center: this.position,
     scrollwheel: true,
     disableDoubleClickZoom: true,
-    zoom: 15,
+    zoom: 14,
     disableDefaultUI: true
   };
 
@@ -31,29 +33,48 @@ export class PropertyLocationComponent implements OnInit {
       apiKey: 'AIzaSyDpNbirRykNtf26goqNIwT4diZcsIP-vy4',
     })
     loader.load().then(() => {
-      this.map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
-        center: this.position,
-        scrollwheel: true,
-        disableDoubleClickZoom: true,
-        zoom: 5,
-        disableDefaultUI: true,
-      });
+      this.map = new google.maps.Map(document.getElementById('map') as HTMLElement, this.options);
     }).catch(error => {
-      console.log('Maps could not load')
+      console.log('Maps could not load', error);
     })
   }
-
 
   geoCode(event: Address): void {
     this.position = {lat: event.geometry.location.lat(), lng: event.geometry.location.lng()}
-    this.geoInformation?.patchValue(this.position);
-    this.addMarker();
+    this.setGeoInfo();
+    this.addMarker(this.position);
+    this.map.setCenter(this.position);
+    this.map.setZoom(14);
   }
 
-  addMarker(): void {
-    const marker = new google.maps.Marker({
-      position: this.position,
-      map: this.map
-    })
+  addMarker(position: google.maps.LatLng | google.maps.LatLngLiteral): void {
+    const marker: google.maps.Marker = new google.maps.Marker({position, map: this.map, draggable: true});
+    this.deletePreviousMarkers();
+    this.markers.push(marker);
+    this.setMapOnAll(this.map);
+    this.addDragEndListener(marker);
+  }
+
+  addDragEndListener(marker: google.maps.Marker): void {
+    google.maps.event.addListener(marker,'dragend', (event) => {
+      this.position = {lat: event.latLng.lat(), lng: event.latLng.lng()};
+      this.map.setCenter(this.position);
+      this.setGeoInfo();
+    });
+  }
+
+   setMapOnAll(map: google.maps.Map | null): void {
+    for (let i = 0; i < this.markers.length; i++) {
+      this.markers[i].setMap(map);
+    }
+  }
+
+  deletePreviousMarkers(): void {
+    this.setMapOnAll(null);
+    this.markers = [];
+  }
+
+  setGeoInfo(): void {
+    this.geoInformation?.setValue(this.position);
   }
 }
