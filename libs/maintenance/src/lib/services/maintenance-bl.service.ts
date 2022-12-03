b62470887811a2e2ed5a57b55236eefeddcdbcfe
thinleyjimmydorji <thinleyjimmy123@gmail.com>
2022-12-03 20:11:44 +0600

    fix(newrentals): maintenance

--- libs/maintenance/src/lib/services/maintenance-bl.service.ts
+++ libs/maintenance/src/lib/services/maintenance-bl.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Maintenance} from "@new-rentals/shared";
+import {MaintenanceCdkDataModel} from "../models/maintenance.models";
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,16 @@ export class MaintenanceBlService {
 
   constructor() { }
 
-  getMaintenanceStateMap(maintenances: Maintenance[]): Record<string, {data: Maintenance[], openToInProgressApiCall?:string, onHoldToInProgressApiCall?: string, apiCall: string }> {
-     const maintenanceMap: Record<string, { data: Maintenance[], openToInProgressApiCall?:string, onHoldToInProgressApiCall?: string, apiCall?: string }> = {
+  getMaintenanceStateMap(maintenances: Maintenance[]): Record<string, MaintenanceCdkDataModel>{
+     const maintenanceMap: Record<string, MaintenanceCdkDataModel> = {
       open: {data: [], apiCall: 'createTenant'},
       on_hold: {data: [], apiCall: 'onHoldMaintenance'},
-      in_progress: {data: [], openToInProgressApiCall: 'workMaintenance', onHoldToInProgressApiCall: 'createMaintenance'},
+      in_progress: {data: [], openToInProgressApiCall: 'workMaintenance', onHoldToInProgressApiCall: 'confirmMaintenance'},
       completed: {data: [], apiCall: 'finishMaintenance'}
     }
     // @ts-ignore
-    return maintenances.forEach(maintenance => maintenanceMap[maintenance.state].data.push(maintenance));
+    maintenances.forEach(maintenance => maintenanceMap[maintenance.state].data.push(maintenance));
+    // @ts-ignore
+    return maintenanceMap;
   }
 }
